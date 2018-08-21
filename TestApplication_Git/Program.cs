e6a55b356869e6106b44ec567713b3708a9423da
Naveen Motwani <naveen.motwani@appliedis.com>
2018-08-21 19:27:33 +0530

    Add project files.

---
+++ TestApplication_Git/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestApplication_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
